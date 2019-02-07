54d049a6912b250fdf7a4412ac3c75284e3144dd
Taha YURDAKUL <tahayurdakul@icloud.com>
2019-02-07 19:20:43 +0300

    Update ACS712_Akim_sensoru_DC_akim_olcme.ino

--- ACS712_Akim_sensoru_DC_akim_olcme.ino
+++ ACS712_Akim_sensoru_DC_akim_olcme.ino
@@ -17,9 +17,9 @@
 **                                                                                                                                  **
 **                                                                                                                                  **
 **                                                                                                                                  **
-**   Not: bu kodda ACS712 Akım sensörü ile DC akım olcumu yaparak serial monitorde deger akısını gormekteyiz.                       **
-**   eger ACS712 modulunu AC akım olcmek icin kullanıcaksanız,acıklamalarda vermis oldugum linkten AC akım olcmek icin              **
-**   kullanılacak olan kodu temin edebilirsiniz.                                                                                    **
+**   Not: bu kodda ACS712 Akim sensörü ile DC akim olcumu yaparak serial monitorde deger akisini gormekteyiz.                       **
+**   eger ACS712 modulunu AC akim olcmek icin kullanicaksaniz https://github.com/tahayrdkl/Arduino vermis oldugum linkten           **
+**   AC akim olcmek icin kullanilacak olan kodu temin edebilirsiniz.                                                                **
 **                                                                                                                                  **
 **************************************************************************************************************************************
 */
@@ -37,10 +37,10 @@ void loop()
   float ortalama = 0;
   for (int i = 0; i < 1000 ; i++)
   {
-    ortalama = ortalama + (.044 * analogRead(A0) - 3.78) / 1000; // ACS712 30A Modeli icin gecerlidir.
-    // "(.044 * analogRead(A0)" yazdığım bu formul ACS712 30A Modeli icin gecerlidir.
-    //  eger modulunuz ACS712 5A modeli ise "(.0264 * analogRead(A0) - 13.51)" bu formulu ,
-    //  ACS712 20A modeli ise "(.19 * analogRead(A0) -25)" bu formulu kullanınız.
+    ortalama = ortalama + (.044 * analogRead(A0) - 3.78) / 1000;
+    // "(.044 * analogRead(A0)" yazdiğim bu formul ACS712 30A Modeli icin gecerlidir.
+    //  eger modulunuz ACS712 5A modeli ise "(.0264 * analogRead(A0) - 13.51)" bu formulu,
+    //  ACS712 20A modeli ise "(.19 * analogRead(A0) -25)" bu formulu kullaniniz.
 
     delay(1);
 
